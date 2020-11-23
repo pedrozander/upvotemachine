35b45dd4cd08974ae8047576dda446468116031e
pedro.zander <peczander@gmail.com>
2020-11-23 14:54:33 -0300

    Config db

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :upvotemachine, Upvotemachine.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "1234",
   database: "upvotemachine_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
